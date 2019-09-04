b2356cbf1f8db00b1f47b71b1b860511b0dd3fbc
trozans-trap <chawlavikalp2000@gmail.com>
2019-09-04 22:50:09 +0530

    js file name changed
    js file name changed , button disable after 20s

---
+++ script.js
@@ -0,0 +1,43 @@
+var image = document.querySelector(".diceimage");
+var dice = [ "dice-1.png","dice-2.png","dice-3.png","dice-4.png","dice-5.png","dice-6.png"];
+var rolldices = document.querySelector(".rolldice");
+var p1scores = document.querySelector(".p1score");
+var newgm = document.querySelector(".newGame");
+var score=0;
+var randomno;
+var t=20;
+var count =document.querySelector(".counter");
+rolldices.addEventListener("click",update);
+
+function update (){ 
+   if(t==20)
+   {
+   	  counter();
+   }
+    randomno= Math.floor( Math.random() * 6);
+	console.log(randomno);
+	image.src=dice[randomno];
+    score+= Number(randomno) + 1;
+    p1scores.textContent=score;	
+}
+
+function counter()
+{ 
+  if(t<=0)
+  {  rolldices.disabled = true;
+  }
+  else 
+  {
+  	t--;
+  	count.textContent=t;
+  }
+  setTimeout(function(){counter();},1000);
+}
+
+newgm.addEventListener("click" , function(){
+	alert("Your Last Game Score is : " + score);
+     p1scores.textContent=0;
+     score=0;
+     t=0;
+     count.textContent=20;
+})
